4eb52c71faee1b29f84c9da5b0445c6d365a6099
Peli, Ferenc <ferenc.peli@accenture.com>
2020-12-09 14:50:03 +0100

    Novenyek órai munka

---
+++ 2020/12_December/EF_novenyek/EntityFrameworkMySQL/Models/Noveny.cs
@@ -0,0 +1,10 @@
+﻿namespace EntityFrameworkMySQL.Models
+{
+    public class Noveny
+    {
+        public int Id { get; set; }
+        public string Nev { get; set; }
+        public string Szin { get; set; }
+        public bool EveloNoveny { get; set; }
+    }
+}
---
+++ 2020/12_December/EF_novenyek/EntityFrameworkMySQL/Program.cs
@@ -0,0 +1,51 @@
+﻿using EntityFrameworkMySQL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkMySQL
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var context = new DatabaseContext();
+
+            var novenyek = new List<Noveny>
+                {
+                    new Noveny{Szin = "sarga", Nev = "citromfa"},
+                    new Noveny{Szin = "barna", Nev = "fűzfa"},
+                    new Noveny{Szin = "piros", Nev = "akácfa"}
+                };
+
+            context.Novenyek.AddRange(novenyek);
+            await context.SaveChangesAsync();
+
+            Console.WriteLine("Adj meg egy növény ID-t:");
+            var id = Convert.ToInt32(Console.ReadLine());
+
+            var entity = context.Novenyek.Find(id);
+
+            if (entity == null)
+            {
+                Console.WriteLine("Nem találtam ilyen növényt!");
+            }
+            else
+            {
+                Console.WriteLine("Add meg a  új nevét: ");
+                var ujnev = Console.ReadLine();
+                entity.Nev = ujnev;
+                await context.SaveChangesAsync();
+            }
+
+            var legnagyobbId = context.Novenyek.OrderByDescending(x => x.Id).First();
+            context.Novenyek.Remove(legnagyobbId);
+            await context.SaveChangesAsync();
+
+            Console.WriteLine($"Még {context.Novenyek.ToList().Count} darab növény van az adatbázisban.");
+
+            Console.ReadLine();
+        }
+    }
+}
