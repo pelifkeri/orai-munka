e1ed07fcec68086a27778d77a2131596eb2a9b45
Peli, Ferenc <ferenc.peli@accenture.com>
2020-12-15 08:56:13 +0100

    DB access, reading and updating a drink

---
+++ 2020/12_December/winforms_italok/winforms-mysql-boilerplate/Form1.cs
@@ -0,0 +1,82 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Windows.Forms;
+
+namespace winforms_mysql_boilerplate
+{
+    public partial class Form1 : Form
+    {
+        private readonly string connectionString;
+
+        public Form1()
+        {
+            InitializeComponent();
+            connectionString = "server=localhost;database=teszt;uid=root";
+
+            listBox1.ValueMember = nameof(Ital.Id);
+            listBox1.DisplayMember = nameof(Ital.Nev);
+        }
+
+        private void OnButtonClick(object sender, System.EventArgs e)
+        {
+            listBox1.Items.Clear();
+            MySqlConnection connection = new MySqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+
+                string sql = "SELECT * FROM italok";
+                MySqlCommand command = new MySqlCommand(sql, connection);
+                MySqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    var ital = new Ital(Convert.ToInt32(reader[0]), reader[1].ToString());
+                    listBox1.Items.Add(ital);
+
+                    Console.WriteLine(ital);
+
+                }
+                reader.Close();
+
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Cannot open connection!");
+            }
+        }
+
+        private void OnListboxSelectedIndexChanged(object sender, EventArgs e)
+        {
+            var kijeloltElem = (Ital)listBox1.SelectedItems[0];
+            textBox1.Text = kijeloltElem.Nev;
+        }
+
+        private void OnAtnevezesClick(object sender, EventArgs e)
+        {
+            var kijeloltElem = (Ital)listBox1.SelectedItems[0];
+            var ujErtek = textBox1.Text;
+
+            MySqlConnection connection = new MySqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+
+                string sql = $"UPDATE italok SET Name = '{ujErtek}' WHERE Id = {kijeloltElem.Id}";
+                MySqlCommand command = new MySqlCommand(sql, connection);
+                MySqlDataReader reader = command.ExecuteReader();
+
+                reader.Close();
+
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Cannot open connection!");
+            }
+        }
+    }
+}
---
+++ 2020/12_December/winforms_italok/winforms-mysql-boilerplate/Ital.cs
@@ -0,0 +1,16 @@
+﻿namespace winforms_mysql_boilerplate
+{
+    public class Ital
+    {
+        public int Id { get; set; }
+        public string Nev { get; set; }
+
+        public string Kombinalt => $"{Id} - {Nev}";
+
+        public Ital(int id, string nev)
+        {
+            Id = id;
+            Nev = nev;
+        }
+    }
+}
