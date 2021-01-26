7edf8e57b2a82b4fde5831a5effcfe8c9afc9356
Peli, Ferenc <ferenc.peli@accenture.com>
2021-01-26 08:22:28 +0100

    WinForms folder dialog app

---
+++ 2020-2021/01_Januar/WinFormsFileDialog/WinFormsFileDialog/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WinFormsFileDialog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSelectFilesButtonClick(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK)
+                {
+                    string[] files = Directory.GetFiles(fbd.SelectedPath);
+                    listBox1.Items.AddRange(files);
+                }
+            }
+        }
+
+        private void OnOpenFileClick(object sender, EventArgs e)
+        {
+            var path = (string)listBox1.SelectedItem;
+            
+            if (path != null)
+            {
+                Process.Start(new ProcessStartInfo(path) { UseShellExecute = true });
+            }
+        }
+    }
+}
