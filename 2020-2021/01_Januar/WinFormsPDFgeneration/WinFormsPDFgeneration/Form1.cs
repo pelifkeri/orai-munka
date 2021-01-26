300462fc747685715e78f886a63fe877cc43fff9
Peli, Ferenc <ferenc.peli@accenture.com>
2021-01-26 08:32:05 +0100

    PDF generálás

---
+++ 2020-2021/01_Januar/WinFormsPDFgeneration/WinFormsPDFgeneration/Form1.cs
@@ -0,0 +1,51 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WinFormsPDFgeneration
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                Document document = new Document(PageSize.A4, 10, 10, 100, 10);
+
+                PdfWriter writer = PdfWriter.GetInstance(document, memoryStream);
+                document.Open();
+
+                Chunk chunk = new Chunk("This is from chunk. ");
+                document.Add(chunk);
+
+                Phrase phrase = new Phrase("This is from Phrase.");
+                document.Add(phrase);
+
+                Paragraph para = new Paragraph("This is from paragraph.");
+                document.Add(para);
+
+                string text = @"you are successfully created PDF file.";
+                Paragraph paragraph = new Paragraph();
+                paragraph.SpacingBefore = 10;
+                paragraph.SpacingAfter = 10;
+                paragraph.Alignment = Element.ALIGN_LEFT;
+                paragraph.Font = FontFactory.GetFont(FontFactory.HELVETICA, 12f, BaseColor.Green);
+                paragraph.Add(text);
+                document.Add(paragraph);
+
+                document.Close();
+                byte[] bytes = memoryStream.ToArray();
+                memoryStream.Close();
+
+                File.WriteAllBytes(@"C:\temp\pelda.pdf", bytes);
+            }
+        }
+    }
+}
