bed4585f7123844490706dc086406296b5e93caf
Peli, Ferenc <ferenc.peli@accenture.com>
2021-02-02 07:54:52 +0100

    WinForms HTTP client

---
+++ 2020-2021/02_Februar/WinFormsHttpClient/WinFormsHttpClient/Form1.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Windows.Forms;
+
+namespace WinFormsHttpClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var httpClient = new HttpClient();
+
+            try
+            {
+                //using (var response = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/users"))
+
+                var user = new User { Id = 123, Name = "Géza" };
+                var json = JsonConvert.SerializeObject(user);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                using (var response = await httpClient.PostAsync("http://localhost:59621/minta", content))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string valasz = await response.Content.ReadAsStringAsync();
+                        //var eredmeny = JsonConvert.DeserializeObject<List<User>>(valasz);
+                        //MessageBox.Show(eredmeny[0].Felhasznalonev);
+                        MessageBox.Show(valasz);
+                    }
+                    else
+                    {
+                        MessageBox.Show(response.StatusCode.ToString());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                throw ex;
+            }
+        }
+    }
+}
---
+++ 2020-2021/02_Februar/WinFormsHttpClient/WinFormsHttpClient/User.cs
@@ -0,0 +1,8 @@
+﻿namespace WinFormsHttpClient
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
