bf469a596f4d54819149d1dcdbbad9ddc825a6b3
Peli, Ferenc <ferenc.peli@accenture.com>
2021-02-03 08:46:04 +0100

    Minta ASP.NET szerver

---
+++ 2020-2021/02_Februar/ElsoWebSzerver/ElsoWebSzerver/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ElsoWebSzerver.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("{id}/minta")]
+        public string Get([FromQuery] string adat, int id)
+        {
+            return $"ID: {id}, Hello {adat}";
+        }
+
+        [HttpPost]
+        public string Post([FromBody] string adat)
+        {
+            return $"Hello {adat}";
+        }
+    }
+}
