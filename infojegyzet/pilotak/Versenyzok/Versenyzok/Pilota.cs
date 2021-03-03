c94360e84d72265bde47dbb5dad9d212ca40ee5c
Peli, Ferenc <ferenc.peli@accenture.com>
2021-03-03 08:07:58 +0100

    Pilotak infojegyzet feladat megoldása

---
+++ infojegyzet/pilotak/Versenyzok/Versenyzok/Pilota.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Versenyzok
+{
+    class Pilota
+    {
+        public string Nev { get; set; }
+        public DateTime SzuletesiDatum { get; set; }
+        public string Nemzetiseg { get; set; }
+        public int Rajtszam { get; set; }
+
+        public Pilota(string sor)
+        {
+            var a = sor.Split(';');
+            Nev = a[0];
+            SzuletesiDatum = Convert.ToDateTime(a[1]);
+            Nemzetiseg = a[2];
+            Rajtszam = a[3] == "" ? 0 : Convert.ToInt32(a[3]);
+        }
+    }
+}
---
+++ infojegyzet/pilotak/Versenyzok/Versenyzok/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Versenyzok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2. feladat
+            List<Pilota> pilotak = new List<Pilota>();
+            var beolvasott = File.ReadAllLines(@"C:\temp\pilotak\pilotak.csv");
+            foreach (var sor in beolvasott.Skip(1))
+            {
+                var pilota = new Pilota(sor);
+                pilotak.Add(pilota);
+            }
+
+            // 3. feladat
+            Console.WriteLine(pilotak.Count);
+
+            // 4. feladat
+            var utolso = pilotak.LastOrDefault();
+            Console.WriteLine(utolso.Nev);
+
+            // 5. feladat
+            var datum = new DateTime(1901, 1, 1);
+            var idosek = pilotak.Where(x => x.SzuletesiDatum < datum).ToList();
+            foreach (var item in idosek)
+            {
+                Console.WriteLine($"{item.Nev} ({item.SzuletesiDatum.ToString("yyyy. MM. dd.")})");
+            }
+
+            // 6. feladat
+            var legkisebb = pilotak
+                .OrderBy(x => x.Rajtszam)
+                .Where(x => x.Rajtszam > 0)
+                .FirstOrDefault();
+            Console.WriteLine(legkisebb.Nemzetiseg);
+
+            // 7. feladat
+            var azonosSorszamok = pilotak
+                .GroupBy(x => x.Rajtszam)
+                .Where(x => x.Count() > 1 && x.Key != 0)
+                .ToList();
+
+            var szamok = azonosSorszamok.Select(x => x.Key).ToList();
+            var eredmeny = String.Join(", ", szamok);
+
+            Console.WriteLine(eredmeny);
+
+            Console.ReadLine();
+        }
+    }
+}
