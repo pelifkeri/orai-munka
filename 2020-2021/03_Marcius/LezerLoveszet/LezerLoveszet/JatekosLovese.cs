56db44deb654cc760f8a016d17ab21013acab6ee
Peli, Ferenc <ferenc.peli@accenture.com>
2021-03-18 08:00:59 +0100

    Lézerlövészet feladat (2018 ősz)

---
+++ 2020-2021/03_Marcius/LezerLoveszet/LezerLoveszet/JatekosLovese.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LezerLoveszet
+{
+    class JatekosLovese
+    {
+        public string Nev { get; set; }
+        public double LovesX { get; set; }
+        public double LovesY { get; set; }
+        public int Sorszam { get; set; }
+
+        public JatekosLovese(string sor, int sorszam)
+        {
+            var a = sor.Split(';');
+            Nev = a[0];
+            LovesX = Convert.ToDouble(a[1]);
+            LovesY = Convert.ToDouble(a[2]);
+            Sorszam = sorszam;
+        }
+
+        public double Tavolsag(double tablaX, double tablaY)
+        {
+            var x = tablaX - LovesX;
+            var y = tablaY - LovesY;
+
+            return Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+        }
+
+        public double Pontszam(double tablaX, double tablaY)
+        {
+            var eredmeny = Math.Round(10 - Tavolsag(tablaX, tablaY), 2);
+            return eredmeny > 0 ? eredmeny : 0;
+        }
+    }
+}
---
+++ 2020-2021/03_Marcius/LezerLoveszet/LezerLoveszet/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace LezerLoveszet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 4. feladat
+            List<JatekosLovese> lista = new List<JatekosLovese>();
+            var beolvasott = File.ReadAllLines(@"C:\temp\Lezer\lovesek.txt");
+
+            var tabla = beolvasott[0].Split(';');
+            double tablaX = Convert.ToDouble(tabla[0]);
+            double tablaY = Convert.ToDouble(tabla[1]);
+
+            for (int i = 1; i < beolvasott.Length; i++)
+            {
+                var loves = new JatekosLovese(beolvasott[i], i);
+                lista.Add(loves);
+            }
+
+            // 5. feladat
+            Console.WriteLine($"Összesen {lista.Count} lövést adtak le a játékosok.");
+
+            // 7. feladat
+            var legpontosabbLoves = lista.OrderBy(x => x.Tavolsag(tablaX, tablaY)).First();
+            Console.WriteLine($"Sorszám: {legpontosabbLoves.Sorszam}, Név: {legpontosabbLoves.Nev}, Távolság: {legpontosabbLoves.Tavolsag(tablaX, tablaY)}");
+
+            // 9. feladat
+            var nullapontos = lista.Where(x => x.Pontszam(tablaX, tablaY) == 0).ToList();
+            Console.WriteLine(nullapontos.Count);
+
+            // 10. feladat
+            var jatekosok = lista.GroupBy(x => x.Nev).ToList();
+            Console.WriteLine($"A játékosok száma: {jatekosok.Count()}");
+
+            // 11. feladat
+            foreach (var nev in jatekosok)
+            {
+                Console.WriteLine($"{nev.Key} - {nev.Count()} db");
+            }
+
+            // 12. feladat
+            var atlagpontok = jatekosok.ToDictionary(x => x.Key, y => y.Average(x => x.Pontszam(tablaX, tablaY)));
+            foreach (var item in atlagpontok)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+
+            // 13. feladat
+            var legmagasabb = atlagpontok.OrderByDescending(x => x.Value).First().Key;
+            Console.WriteLine($"A játék nyertese: {legmagasabb}");
+
+
+            Console.ReadLine();
+        }
+    }
+}
