194f8fdac0988c87d8cf7abe684934029aa2fbe0
Ferenc Péli <ferenc.peli@axzoom.de>
2021-04-27 08:48:39 +0200

    Tesztverseny feladat megoldása

---
+++ 2020-2021/04_Aprilis/tesztverseny/tesztverseny/Eredmeny.cs
@@ -0,0 +1,45 @@
+﻿namespace tesztverseny
+{
+    class Eredmeny
+    {
+        public string Versenyzo { get; set; }
+        public string Valaszok { get; set; }
+
+        public Eredmeny(string sor)
+        {
+            var a = sor.Split();
+            Versenyzo = a[0];
+            Valaszok = a[1];
+        }
+
+        public int Pontszam(string megoldokulcs)
+        {
+            int pontszamok = 0;
+
+            for (int i = 0; i < Valaszok.Length; i++)
+            {
+                if (Valaszok[i] == megoldokulcs[i])
+                {
+                    if (i == 13)
+                    {
+                        pontszamok += 6;
+                    }
+                    else if (i >= 10 && i <= 12)
+                    {
+                        pontszamok += 5;
+                    }
+                    else if (i >= 5 && i <= 9)
+                    {
+                        pontszamok += 4;
+                    }
+                    else
+                    {
+                        pontszamok += 3;
+                    }
+                }
+            }
+
+            return pontszamok;
+        }
+    }
+}
---
+++ 2020-2021/04_Aprilis/tesztverseny/tesztverseny/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace tesztverseny
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. feladat
+            List<Eredmeny> lista = new List<Eredmeny>();
+            var beolvasott = File.ReadAllLines(@"C:\temp\tesztverseny\valaszok.txt");
+            var helyesValaszok = beolvasott[0];
+
+            foreach (var sor in beolvasott.Skip(1))
+            {
+                var eredmeny = new Eredmeny(sor);
+                lista.Add(eredmeny);
+            }
+
+            // 2.feladat
+            Console.WriteLine($"A versenyen {lista.Count} db versenyző vett részt.");
+
+            // 3. feladat
+            var azonosito = Console.ReadLine();
+            var egyeniEredmeny = lista.Where(x => x.Versenyzo == azonosito).FirstOrDefault();
+            Console.WriteLine($"{egyeniEredmeny.Versenyzo} - {egyeniEredmeny.Valaszok}");
+
+            // 4. feladat
+            Console.WriteLine(helyesValaszok);
+
+            string str = "";
+            for (int i = 0; i < helyesValaszok.Length; i++)
+            {
+                str += helyesValaszok[i] == egyeniEredmeny.Valaszok[i] ? "+" : " ";
+            }
+            Console.WriteLine(str);
+
+            // 5. feladat
+            var sorszam = Convert.ToInt32(Console.ReadLine());
+            var helyesValasz = helyesValaszok[sorszam - 1];
+            var valaszok = lista.Select(x => x.Valaszok[sorszam - 1]).ToList();
+            var helyesValaszadok = valaszok.Where(x => x == helyesValasz).ToList();
+
+            double megoszlas = Math.Round(Convert.ToDouble(helyesValaszadok.Count) / Convert.ToDouble(valaszok.Count), 4) * 100;
+            Console.WriteLine($"A(z) {sorszam}. kérdésre {helyesValaszadok.Count} versenyző {megoszlas}%-a adott jó választ");
+
+            // 6. feladat
+            List<string> eredmenyek = new List<string>();
+            foreach (var eredmeny in lista)
+            {
+                string sor = $"{eredmeny.Versenyzo} {eredmeny.Pontszam(helyesValaszok)}";
+                eredmenyek.Add(sor);
+            }
+            File.WriteAllLines(@"C:\temp\tesztverseny\pontok.txt", eredmenyek);
+
+            // 7. feladat
+            var vegeredmeny = lista
+                .GroupBy(x => x.Pontszam(helyesValaszok))
+                .OrderByDescending(x => x.First().Pontszam(helyesValaszok))
+                .Take(3)
+                .ToList();
+
+            int helyezes = 1;
+            foreach (var versenyzok in vegeredmeny)
+            {
+                foreach (var item in versenyzok)
+                {
+                    Console.WriteLine($"({helyezes}. díj) ({item.Pontszam(helyesValaszok)} pont): {item.Versenyzo}");
+                }
+                helyezes += 1;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
