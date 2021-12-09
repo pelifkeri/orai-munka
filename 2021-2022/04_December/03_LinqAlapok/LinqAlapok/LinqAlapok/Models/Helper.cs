380d2b995916423009587c2a8f4af659fe37e298
Ferenc Péli <ferenc.peli@outlook.com>
2021-12-09 09:46:21 +0100

    Linq alapok

---
+++ 2021-2022/04_December/03_LinqAlapok/LinqAlapok/LinqAlapok/Models/Helper.cs
@@ -0,0 +1,15 @@
+﻿using LinqAlapok22;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqAlapok.Models
+{
+    class Helper
+    {
+        public Helper()
+        {
+            var person = new Person();
+        }
+    }
+}
---
+++ 2021-2022/04_December/03_LinqAlapok/LinqAlapok/LinqAlapok/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqAlapok22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> lista = new List<Person>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                var person = new Person
+                {
+                    Id = i,
+                    Name = Faker.Name.FullName(),
+                    Felnott = i % 3 == 0
+                };
+                lista.Add(person);
+            }
+
+            var abc = lista
+                .OrderBy(x => x.Name)
+                .ToList();
+
+            var csoportok = lista
+                .GroupBy(x => x.Felnott)
+                .ToList();
+
+            var felnottekCsokkenoNevsorban = lista
+                .Where(x => x.Felnott)
+                .OrderByDescending(x => x.Name)
+                .ToList();
+
+            Console.ReadLine();
+        }
+    }
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Felnott { get; set; }
+    }
+}
