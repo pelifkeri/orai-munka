d5f83dbb3591d9cbeeceaf07ab26cc02cec1bcaa
Ferenc Péli <ferenc.peli@outlook.com>
2021-12-16 09:48:12 +0100

    File muveletek

---
+++ 2021-2022/04_December/06_FileMuveletek/FileMuveletek/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace FileMuveletek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var beolvasott = File.ReadAllLines(@"C:\temp\adat.txt");
+            List<Person> lista = new List<Person>();
+
+            foreach (string sor in beolvasott)
+            {
+                var person = new Person(sor);
+                lista.Add(person);
+            }
+
+            List<string> kiirando = new List<string>();
+            kiirando.Add("Ez a .txt fájl első sora.");
+            foreach (var person in lista)
+            {
+                kiirando.Add($"{person.Name} jelenleg {person.Age} éves.");
+            }
+
+            File.WriteAllLines(@"C:\temp\kiirtadat.txt", kiirando);
+        }
+    }
+
+    class Person
+    {
+        public Person(string sor)
+        {
+            var adat = sor.Split(";");
+            Id = Convert.ToInt32(adat[0]);
+            Name = adat[1];
+            Age = Convert.ToInt32(adat[2]);
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
