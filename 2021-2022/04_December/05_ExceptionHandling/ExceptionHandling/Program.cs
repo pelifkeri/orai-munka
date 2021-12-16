5168beb1ae8f9638345f5ee1ee38e9322eb5e72a
Ferenc Péli <ferenc.peli@outlook.com>
2021-12-16 09:19:21 +0100

    Exception handling

---
+++ 2021-2022/04_December/05_ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lista = new List<string>();
+
+            var elso = lista.FirstOrDefault() ?? "default érték";
+
+            try
+            {
+                elso = lista.First();
+                Fuggveny();
+            }
+            catch (AggregateException ex)
+            {
+                elso = "valami default érték";
+            }
+            catch (Exception ex)
+            {
+                elso = "valami default érték";
+            }
+
+            Console.WriteLine($"Érték: {elso}");
+        }
+
+        static void Fuggveny()
+        {
+            try
+            {
+                List<string> lista = new List<string>();
+                var elso = lista.First();
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("Ó-ó", ex);
+            }
+        }
+    }
+}
